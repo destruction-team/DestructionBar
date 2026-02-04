b2a44f0949185963cef86478beb55e85a4f53bab
envizar <envizar@meza.one>
2026-02-04 12:27:46 +0300

    init plugin

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.gradleupShadow)
+    alias(libs.plugins.runPaperTask)
+}
+
+group = "cc.dstm"
+version = "0.0"
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+
+    compileOnly(libs.kotlin.stdlib)
+}
+
+tasks {
+    runServer {
+        minecraftVersion(libs.versions.paper.server.get())
+    }
+
+    // output jar with shaded libs
+    shadowJar {
+        archiveClassifier = ""
+        manifest { attributes["paperweight-mappings-namespace"] = "mojang" }
+    }
+    jar { enabled = false }
+    build {dependsOn("shadowJar") }
+
+    // process replacements
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/kotlin/cc/dstm/destructionBar/Main.kt
@@ -0,0 +1,14 @@
+package cc.dstm.destructionBar
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
